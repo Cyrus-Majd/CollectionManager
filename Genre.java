e36c8fc93908e2cbdd3d4387f8aa5490b36aa218
Nikodem Kisielewski <75518131+nikodem-kisielewski@users.noreply.github.com>
2021-09-20 18:31:21 -0400

    Album genre enum

---
+++ Genre.java
@@ -0,0 +1,11 @@
+/** Defines the genres of the album collection.
+ * @author Cyrus Majd
+ * @author Nikodem Kisielewski
+ */
+public enum Genre {
+	POP,
+	COUNTRY,
+	CLASSICAL,
+	JAZZ,
+	UNKNOWN;
+}
