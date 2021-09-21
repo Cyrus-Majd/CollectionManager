00ba3aedbaeedadb2fd4ed638b68f5417d7cd239
Nikodem Kisielewski <75518131+nikodem-kisielewski@users.noreply.github.com>
2021-09-20 21:44:22 -0400

    Maybe try to optimize isValid and compareTo

--- Date.java
+++ Date.java
@@ -1,4 +1,5 @@
-// TODO implement isValid and compareTo. Maybe implement other methods
+import java.util.Calendar;
+import java.util.StringTokenizer;
 
 /** 
  * This class defines the Date data type.
@@ -12,14 +13,34 @@ public class Date implements Comparable<Date> {
 	private int month;
 	private int year;
 	
+	// Numbers used for calculating date validity
+	private static final int QUADRENNIAL = 4;
+	private static final int CENTENNIAL = 100;
+	private static final int QUARTERCENNIAL = 400;
+	private static final int THE_EIGHTIES = 1980;
+	
 	/** Default constructor. Creates a Date instance with today's date as the parameters.*/
-	public Date() {}
+	public Date() {
+		// Create a calendar instance to get date parameters
+		Calendar calendar = Calendar.getInstance();
+		
+		// Get today's month, day, and year
+		month = calendar.get(Calendar.MONTH) + 1;	// +1 since January is indexed as 0
+		day = calendar.get(Calendar.DAY_OF_MONTH);
+		year = calendar.get(Calendar.YEAR);
+	}
 	
 	/** Takes in a date string and creates an instance of Date.
 	 * @param date a date string in the form "mm/dd/yyyy".
 	 */
 	public Date(String date) {
+		// Constructs a StringTokenizer to individually set the Date parameters
+		StringTokenizer dateTokens = new StringTokenizer(date, "/");
 		
+		// Set each date parameter
+		month = Integer.parseInt(dateTokens.nextToken());
+		day = Integer.parseInt(dateTokens.nextToken());
+		year = Integer.parseInt(dateTokens.nextToken());
 	}
 	
 	/** Checks to see if a given date us valid. A date cannot be
@@ -31,15 +52,92 @@ public class Date implements Comparable<Date> {
 	 * @return True or False depending on whether the date is considered valid.
 	 */
 	public boolean isValid() {
+		// Create a calendar instance to get today's date parameters
+		Calendar calendar = Calendar.getInstance();
+		
+		// Check if the date is after today
+		if (year > calendar.get(Calendar.YEAR) || month > calendar.get(Calendar.MONTH) + 1 || day > calendar.get(Calendar.DAY_OF_MONTH)) {
+			return false;
+		}
+		
+		// Check if the month is valid
+		if (month < 1 || month > 12) {
+			return false;
+		}
+		
+		// Check if the day is at least 1 and no greater than 
+		if (day < 1 || day > 31) {
+			return false;
+		}
+		
+		// Check if the year is before 1980
+		if (year < THE_EIGHTIES) {
+			return false;
+		}
+		
+		// If it is February, check for leap year validity
+		if (month == Calendar.FEBRUARY + 1) {
+			// Check if it is a leap year
+			boolean isLeapYear;
+			if (year % QUADRENNIAL == 0) {
+				if (year % CENTENNIAL == 0) {
+					if (year % QUARTERCENNIAL == 0) {
+						isLeapYear = true;
+					} else {
+						isLeapYear = false;
+					}
+				} else {
+					isLeapYear = true;
+				}
+			} else {
+				isLeapYear = false;
+			}
+			// If it is not a leap year and the date is Feb 29, the date is invalid
+			if (!isLeapYear && day == 29) {
+				return false;
+			}
+		}
+		
+		// Check to see if the other month and day combinations are valid
+		if (day > 30 && (month == Calendar.APRIL + 1 || month == Calendar.JUNE + 1 || month == Calendar.SEPTEMBER + 1 || month == Calendar.NOVEMBER + 1)) {
+			return false;
+		}
+		
+		// Return true if the date passes all of the checks
 		return true;
 	}
 	
 	/** Overrides the compareTo method and compares 2 dates.
 	 * @param date an instance of Date.
+	 * @return 0, 1, or 2. If this Date is more recent, returns 0. If the input Date is more recent, return
 	 */
 	@Override
 	public int compareTo(Date date) {
-		return 0;
+		// Check if this date's year is more recent
+		if (this.year > date.year) {
+			return 0;
+		}
+		// Check if the years are equal
+		if (this.year == date.year) {
+			// Check if this date's month is more recent
+			if (this. month > date.month) {
+				return 0;
+			}
+			// Check if the months are equal
+			if (this.month == date.month) {
+				// Check if this date's day is more recent
+				if (this.day > date.day) {
+					return 0;
+				}
+				// The dates are the exact same
+				if (this.day == date.day) {
+					return 2;
+				}
+				return 1;
+			}
+			return 1;
+		}
+		return 1;
 	}
 	
 	/** Overrides the toString method and returns the Date instance as a string.
@@ -47,25 +145,25 @@ public class Date implements Comparable<Date> {
 	 */
 	@Override
 	public String toString() {
-		return Integer.toString(day) + "/" + Integer.toString(month) + "/" + Integer.toString(year);
+		return Integer.toString(month) + "/" + Integer.toString(day) + "/" + Integer.toString(year);
 	}
 	
 	/** Returns the day of a Date instance.
-	 * @return The integer representation of a day.
+	 * @return The integer representation the release day.
 	 */
 	public int getDay() {
 		return day;
 	}
 	
 	/** Returns the month of a date instance.
-	 * @return The integer representation of a month.
+	 * @return The integer representation of the release month.
 	 */
 	public int getMonth() {
 		return month;
 	}
 	
 	/** Returns the year of a date instance.
-	 * @return The integer representation of a year.
+	 * @return The integer representation of the release year.
 	 */
 	public int getYear() {
 		return year;
