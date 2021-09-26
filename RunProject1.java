e2493254a1e15d7e715633a5006f9c9786cf2869
Nikodem Kisielewski <75518131+nikodem-kisielewski@users.noreply.github.com>
2021-09-26 17:04:55 -0400

    Create RunProject1.java

---
+++ RunProject1.java
@@ -0,0 +1,11 @@
+/** Class that runs the collection manager program.
+ * 
+ * @author Cyrus Majd
+ * @author Nikodem Kisielewski
+ *
+ */
+public class RunProject1 {
+	public static void main(String args[]) {
+		new CollectionManager().run();
+	}
+}
