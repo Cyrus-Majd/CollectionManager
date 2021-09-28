93b196a3519511766830d1ce65682cf5d9831501
Nikodem Kisielewski <75518131+nikodem-kisielewski@users.noreply.github.com>
2021-09-27 22:12:23 -0400

    Update Collection.java

--- Collection.java
+++ Collection.java
@@ -1,10 +1,21 @@
-// @author Cyrus Majd
-// @author Nikodem Kisielewski
-
+/** Class that defines the array list that stores albums and provides methods to organize and manipulate the collection.
+ * 
+ * @author Cyrus Majd
+ * @author Nikodem Kisielewski
+ *
+ */
 public class Collection {
+	/** An array containing all of the albums in the collection.*/
     private Album[] albums = new Album[4];
-    private int numAlbums = 0;  // number of albums currently in the collection
+    /** The number of albums currently in the collection.*/
+    private int numAlbums = 0;
 
+    /** Method that finds the specified album object in the collection. Returns the index of the album or -1 if the album could not be found.
+     * 
+     * @param album an album object that
+     * @return the index of the desired album object or -1 if the album could not be found
+     * 
+     */
     private int find(Album album) { // find the album index or return "NOT_FOUND". for NOT_FOUND I'm just gonna return -1.
         for (int i = 0; i < albums.length; i++) {
             if (album.equals(albums[i])) {
@@ -14,6 +25,7 @@ public class Collection {
         return -1;
     }
 
+    /** Increases the capacity of the album collection by 4.*/
     private void grow() {   // increase the capacity of the arraylist by 4
         Album[] newAlbums = new Album[albums.length + 4];
         for (int i = 0; i < newAlbums.length; i++) {
@@ -26,6 +38,13 @@ public class Collection {
         albums = newAlbums;
     }
 
+    /** Adds an album to the collection. If the collection is full, will call the grow method to create more space for the new album.
+     * If the album is already in the collection, returns false and does not add the album.
+     * 
+     * @param album the album object to be added
+     * @return True if the album is added successfully; False if the album is already in the collection
+     * 
+     */
     public boolean add(Album album) {
         int searchResult = find(album);
         if (searchResult == -1) {   // The element is not found, which means we are clear to add it.
@@ -48,6 +67,12 @@ public class Collection {
         }
     }
 
+    /** Removes the specified album from the collection. Returns true if successful or false if the album could not be removed.
+     * 
+     * @param album the album object to be removed
+     * @return True if the album is removed successfully; False if the album could not be found
+     * 
+     */
     public boolean remove(Album album) {
         int searchResult = find(album);
         if (searchResult == -1) {
@@ -63,6 +88,12 @@ public class Collection {
         }
     }
 
+    /** Finds the specified album in the collection and sets its availability to not available. The album has been "lended out."
+     * 
+     * @param album the album object to be lended out
+     * @return True if the album is lended out successfully; False if the album could not be found
+     * 
+     */
     public boolean lendingOut(Album album) {    // set to not available
         int index = find(album);
         if (index > -1) {
@@ -72,6 +103,12 @@ public class Collection {
         return false;
     }
 
+    /** Finds the specified album in the collection and sets its availability to available. The album has been "returned."
+     * 
+     * @param album the album object to be returned
+     * @return True if the album is returned successfully; False if the album could not be found
+     * 
+     */
     public boolean returnAlbum(Album album) {   // set to available
         int index = find(album);
         if (index > -1) {
@@ -81,6 +118,7 @@ public class Collection {
         return false;
     }
 
+    /** Prints all of the albums in the collection in no particular order.*/
     public void print() {   // display the list without specifying the order
         if (numAlbums == 0) {
             System.out.println("The collection is empty!");
@@ -92,6 +130,7 @@ public class Collection {
         System.out.println("*End of list");
     }
 
+    /** Prints all of the albums in the collection in order of release date.*/
     public void printByReleaseDate() {
         if (numAlbums == 0) {
             System.out.println("The collection is empty!");
@@ -100,6 +139,7 @@ public class Collection {
 
     }
 
+    /** Prints all of the albums in the collection in order of genre.*/
     public void printByGenre() {
         if (numAlbums == 0) {
             System.out.println("The collection is empty!");
